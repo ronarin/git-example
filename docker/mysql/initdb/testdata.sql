ab96f668d68bd2278e9cecb3b43dfa1b68a70205
uri417 <y.m.n.k.t30113@gmail.com>
2020-03-08 22:47:45 +0900

    DB設定（docker）

---
+++ docker/mysql/initdb/testdata.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (user_id,name,email) VALUES (1, 'MOMO','MOMO@mail.co.jp');
+INSERT INTO users (user_id,name,email) VALUES (2, 'SORA','SORA@mail.co.jp');
+INSERT INTO users (user_id,name,email) VALUES (3, 'SHIINA','SHIINA417@mail.co.jp');
